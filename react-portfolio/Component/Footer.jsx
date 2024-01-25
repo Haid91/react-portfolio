275473bb0886b4ec0740b1c270a0f975928e9616
Haider Shahid <haidershahid3.16@live.com>
2024-01-25 16:07:27 +1100

    third commit

---
+++ react-portfolio/Component/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <a href="https://github.com/Haid91/react-portfolio">GitHub</a>
+      {/* Add other platform links */}
+    </footer>
+  );
+};
+
+export default Footer;

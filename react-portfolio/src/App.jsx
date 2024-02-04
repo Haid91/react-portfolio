66721371df41a94e28f2c3757e77a9a96737f0e2
Haider Shahid <haidershahid3.16@live.com>
2024-02-04 18:05:06 +1100

    final commit

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,13 +1,29 @@
 import { Outlet } from 'react-router-dom';
 import Header from './Component/Header';
+import { HashRouter } from "react-router-dom";
+import About from './pages/About';
 
+import  Contact from './pages/Contact';
 
+import Portfolio from './pages/Portfolio';
+
+import Resume from './pages/Resume';
 function App() {
   // The Outlet component will conditionally swap between the different pages according to the URL
   return (
     <>
-      <Header/>
-      <Outlet />
+    <Header/>
+    <HashRouter basename="/app">
+      <Routes>
+        <Route path="/" element={About}/> {/* 👈 Renders at /#/app/ */}
+        <Route path="/portfolio" element={Portfolio}/> {/* 👈 Renders at /#/app/ */}
+        <Route path="/resume" element={Resume}/> {/* 👈 Renders at /#/app/ */}
+        <Route path="/contact" element={Contact}/> {/* 👈 Renders at /#/app/ */}
+
+      </Routes>
+    </HashRouter>
+      
+      
     </>
   );
 }
